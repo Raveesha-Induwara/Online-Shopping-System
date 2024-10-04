74186851f6bb6c2f9f2d7519eeaa2998d5c43832
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-10-04 22:01:02 +0530

    Added newly created src folders into each modules

---
+++ oss-backend/delivery/src/test/java/com/example/user/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
