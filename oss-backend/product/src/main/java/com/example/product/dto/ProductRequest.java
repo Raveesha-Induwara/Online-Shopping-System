a4f94093b59df2c94a6421688495207a053f124c
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-10-18 01:51:54 +0530

    Product request and respond dto Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- oss-backend/product/src/main/java/com/example/product/dto/ProductRequest.java
+++ oss-backend/product/src/main/java/com/example/product/dto/ProductRequest.java
@@ -1,4 +1,18 @@
 package com.example.product.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductRequest {
+    //Field Representing for product request
+    private String productName;
+    private String productDescription;
+    private String productCategory;
+    private int productPrice;
 }
