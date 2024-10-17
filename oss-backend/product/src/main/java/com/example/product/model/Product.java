b0da4c70dc0655967c876c9cf8edbc29f1ec2277
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-10-18 01:52:09 +0530

    product table model Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- oss-backend/product/src/main/java/com/example/product/model/Product.java
+++ oss-backend/product/src/main/java/com/example/product/model/Product.java
@@ -1,4 +1,26 @@
 package com.example.product.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+//Table name has assigned
+@Table(name = "product")
 public class Product {
+    @Id
+    //Auto generating id for the item
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //Column values for the product table
+    private long id;
+    private String productName;
+    private String productDescription;
+    private String productCategory;
+    private int productPrice;
 }
