b6f7c12dac1540a7a9f47ccef72ba1b6c2d8146f
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-10-21 01:15:12 +0530

    Integrate Swagger 3 with Spring Cloud

---
+++ oss-backend/apigateway/src/main/java/com/example/apigateway/routes/Routes.java
@@ -0,0 +1,36 @@
+package com.example.apigateway.routes;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
+
+@EnableDiscoveryClient
+@Configuration
+@Slf4j
+public class Routes {
+    
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        log.info("Configuring gateway routes...");
+        
+        RouteLocator routeLocator = builder.routes()
+                    .route("auth-service", r -> r.path("/auth-service/v3/api-docs").uri("lb://auth"))
+                    .route("cart-service", r -> r.path("/cart-service/v3/api-docs").uri("lb://cart"))
+                    .route("category-service", r -> r.path("/category-service/v3/api-docs").uri("lb://category"))
+                    .route("delivery-service", r -> r.path("/delivery-service/v3/api-docs").uri("lb://delivery"))
+                    .route("inventory-service", r -> r.path("/inventory-service/v3/api-docs").uri("lb://inventory"))
+                    .route("order-service", r -> r.path("/order-service/v3/api-docs").uri("lb://order"))
+                    .route("payment-service", r -> r.path("/payment-service/v3/api-docs").uri("lb://payment"))
+                    .route("product-service", r -> r.path("/product-service/v3/api-docs").uri("lb://product"))
+                    .route("user-service", r -> r.path("/user-service/v3/api-docs").uri("lb://user"))
+                    
+                    .route("product-service", r -> r.path("/api/v1/products").uri("lb://product"))
+                    .build();
+        
+        log.info("Gateway routes configured successfully.");
+        return routeLocator;
+    }
+}
---
+++ oss-backend/payment/src/main/java/com/example/user/config/OpenApiConfigs.java
@@ -0,0 +1,27 @@
+package com.example.user.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenApiConfigs {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Development");
+        
+        Info information = new Info()
+                                   .title("Payment Service")
+                                   .version("v1.0.0")
+                                   .description("Documentation Payment Service");
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
+}
