ae7251045386c5538b184b9379844ff7cbd4fe4e
6614Kavya <109873890+6614Kavya@users.noreply.github.com>
2024-11-17 11:32:28 +0530

    PrimaryButton implemented

--- oss-frontend/src/App.tsx
+++ oss-frontend/src/App.tsx
@@ -3,14 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { NavBar } from "./Components/NavBar";
+import { PrimaryButton } from "./Components/PrimaryButton";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      {" "}
-      <NavBar />{" "}
+      {/* {" "}
+      <NavBar />{" "} */}
+      <PrimaryButton title="submit" />
     </div>
   );
 }
