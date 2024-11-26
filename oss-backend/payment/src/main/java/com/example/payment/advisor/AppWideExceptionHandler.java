4ffc6837847d6d09c8741869f75458794662ca29
AvishkaWPA <avishkabiz123@gmail.com>
2024-11-27 01:38:33 +0530

    handle exception

--- oss-backend/payment/src/main/java/com/example/payment/advisor/AppWideExceptionHandler.java
+++ oss-backend/payment/src/main/java/com/example/payment/advisor/AppWideExceptionHandler.java
@@ -1,7 +1,26 @@
 package com.example.payment.advisor;
 
+import com.example.payment.dto.response.StandardResponse;
+import com.example.payment.exception.PaymentException;
+import com.example.payment.exception.PaymentProcessingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class AppWideExceptionHandler {
+
+
+    @ExceptionHandler(PaymentException.class)
+    public ResponseEntity<StandardResponse> handlePaymentException(PaymentException e){
+        return new ResponseEntity<StandardResponse>(new StandardResponse(400,"Error",e.getMessage()),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PaymentProcessingException.class)
+    public ResponseEntity<StandardResponse> handlePaymentProcessingException(PaymentProcessingException e){
+        return new ResponseEntity<StandardResponse>(new StandardResponse(500,"Error",e.getMessage()),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
 }
