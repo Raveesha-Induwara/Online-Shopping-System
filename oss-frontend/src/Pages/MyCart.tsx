cc5f8ee5a10ba782d0c1f2054b14608ff9de69c9
6614Kavya <109873890+6614Kavya@users.noreply.github.com>
2024-11-28 01:31:11 +0530

    Implemented update quantity and deleting an item

--- oss-frontend/src/Pages/MyCart.tsx
+++ oss-frontend/src/Pages/MyCart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   DataGrid,
   GridColDef,
@@ -6,6 +6,8 @@ import {
   GridRowsProp,
 } from "@mui/x-data-grid";
 import {
+  Box,
+  IconButton,
   Paper,
   Table,
   TableBody,
@@ -13,26 +15,37 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
 } from "@mui/material";
 import { PrimaryButton } from "../Components/PrimaryButton";
+import { Add, Remove, DeleteForeverOutlined } from "@mui/icons-material";
 
 function createData(
+  id: number,
   product: string,
   name: string,
   description: string,
   quantity: number
 ) {
-  return { product, name, description, quantity };
+  return { id, product, name, description, quantity };
 }
 
-const rows = [
+const initialRows = [
   createData(
+    1,
     "././src/assets/Images/maxi3.jpg",
     "Slate Maxi - Gown",
     "Slate Maxi - Gown",
     1
   ),
   createData(
+    2,
     "././src/assets/Images/maxi2.jpg",
     "Slate Maxi - Gown2",
     "Slate Maxi - Gown2",
@@ -41,6 +54,34 @@ const rows = [
 ];
 
 export default function MyCart() {
+  const [rows, setRows] = useState(initialRows);
+  const [open, setOpen] = useState(false);
+
+  //update quantity of each product in the cart
+  const updateQuantity = (id: number, increment: number) => {
+    setRows((prevRows) =>
+      prevRows.map((rowObj) =>
+        rowObj.id === id
+          ? { ...rowObj, quantity: Math.max(0, rowObj.quantity + increment) }
+          : rowObj
+      )
+    );
+    console.log("rows", rows);
+  };
+
+  const deleteProductFromCart = (id: number) => {
+    setRows((prevRows) => prevRows.filter((rowObj) => rowObj.id !== id));
+    handleClose();
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <TableContainer component={Paper}>
@@ -65,9 +106,65 @@ export default function MyCart() {
                 </TableCell>
                 <TableCell>{row.name}</TableCell>
                 <TableCell>{row.description}</TableCell>
-                <TableCell>{row.quantity}</TableCell>
                 <TableCell>
-                  <PrimaryButton title="Delete" />
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexGrow: 1,
+                      flexDirection: "row",
+                      // justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <IconButton
+                      size="small"
+                      onClick={() => updateQuantity(row.id, -1)}
+                    >
+                      <Remove />
+                    </IconButton>
+                    <Typography>{row.quantity}</Typography>
+                    <IconButton
+                      size="small"
+                      onClick={() => updateQuantity(row.id, 1)}
+                    >
+                      <Add />
+                    </IconButton>
+                  </Box>
+                </TableCell>
+                <TableCell>
+                  <IconButton
+                    size="small"
+                    color="error"
+                    onClick={() => handleOpen()}
+                  >
+                    <DeleteForeverOutlined />
+                  </IconButton>
+                  <Dialog open={open} onClose={() => handleClose()}>
+                    <DialogTitle>
+                      {"You are about to delete an item"}
+                    </DialogTitle>
+                    <DialogContent>
+                      <DialogContentText>
+                        Are you sure you want to delete this item?
+                      </DialogContentText>
+                    </DialogContent>
+
+                    <DialogActions>
+                      <Button
+                        variant="outlined"
+                        onClick={() => deleteProductFromCart(row.id)}
+                      >
+                        Confirm
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="error"
+                        onClick={() => handleClose()}
+                      >
+                        Cancel
+                      </Button>
+                    </DialogActions>
+                  </Dialog>
                 </TableCell>
               </TableRow>
             ))}
