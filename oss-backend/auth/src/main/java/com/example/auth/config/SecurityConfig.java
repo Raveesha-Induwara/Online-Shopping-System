ff2b6f69f3d7a3d569673f48c5eb1d76ffe099af
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-11-30 23:25:49 +0530

    Create two custom UserDetailsService implementations

--- oss-backend/auth/src/main/java/com/example/auth/config/SecurityConfig.java
+++ oss-backend/auth/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.example.auth.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,12 +24,21 @@ import java.util.Collections;
 @Configuration
 public class SecurityConfig {
     
+    @Autowired
+    @Qualifier("customUserDetailsService")
+    private UserDetailsService customUserDetailsService;
+    
+    @Autowired
+    @Qualifier("adminUserDetailsService")
+    private UserDetailsService adminUserDetailsService;
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.sessionManagement(Management -> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(Authorize -> Authorize.requestMatchers(
                         "api/v1/auth/login/client",
                         "api/v1/auth/login/admin",
+                        "api/v1/auth/signup/admin",
                         "api/v1/auth/signup/client",
                         "api/v1/auth/signup/client/create-otp",
                         "api/v1/auth/request-otp/client",
@@ -59,4 +74,12 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration, AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(adminUserDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
 }
--- oss-backend/auth/src/main/java/com/example/auth/service/CustomerUserDetailsService.java
+++ oss-backend/auth/src/main/java/com/example/auth/service/CustomerUserDetailsService.java
@@ -4,6 +4,7 @@ import com.example.auth.repo.ClientCredentialRepo;
 import com.example.auth.model.ClientCredential;
 import com.example.user.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,25 +16,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
-    
+@Qualifier("customUserDetailsService")
+public class CustomerUserDetailsService implements UserDetailsService {
     private final WebClient userWebClient;
     
     @Autowired
     private ClientCredentialRepo clientCredentialRepo;
     
-    public CustomUserDetailsService(WebClient userWebClient) {
+    public CustomerUserDetailsService(WebClient userWebClient) {
         this.userWebClient = userWebClient;
     }
     
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Customer userDetails = userWebClient.post()
-                                .uri(uriBuilder -> uriBuilder.path("/getuser").build())
-                                .bodyValue(email)
-                                .retrieve()
-                                .bodyToMono(Customer.class)
-                                .block();
+        Customer userDetails = userWebClient.get()
+                                      .uri(uriBuilder -> uriBuilder
+                                      .path("/getuser")
+                                      .queryParam("email", email)
+                                      .build())
+                                      .retrieve()
+                                      .bodyToMono(Customer.class)
+                                      .block();
         
         if (userDetails == null) {
             throw new UsernameNotFoundException("user not found with email " + email);
@@ -44,6 +47,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         //      In here I'm not making this application role based therefore, I don't need Authority
         //      because of that, create empty granted authority
         List<GrantedAuthority> authorities = new ArrayList<>();
+//        authorities.add(new Role(user.getUserType().name()));
         return new org.springframework.security.core.userdetails.User(userDetails.getEmail(), user.getPassword(), authorities);
     }
 }
