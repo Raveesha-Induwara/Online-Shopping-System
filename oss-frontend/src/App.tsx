00f3b56dbfed8547139c61ac267a1beb80c1908d
HasiniTharushika <143073840+HasiniTharushika@users.noreply.github.com>
2024-12-02 01:53:03 +0530

    registration and login pages added

--- oss-frontend/src/App.tsx
+++ oss-frontend/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Registrationform from "./Pages/RegisterCustomer";
+import LoginForm from "./Pages/UserLogin";
+import RegistrationOTP from "./Pages/RegistrationOTP";
+import PasswordResetOTP from "./Pages/RestPasswordOTP";
+import PasswordReset from "./Pages/PasswordReset";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterCustomer from "./Pages/RegisterCustomer";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<RegisterCustomer />} />
+        <Route path="/registrationOTP" element={<RegistrationOTP />} />
+        <Route path="/loginForm" element={<LoginForm />} />
+
+        <Route path="/loginForm" element={<LoginForm />} />
+        <Route path="/passwordReset" element={<PasswordReset />} />
+        <Route path="/passwordResetOTP" element={<PasswordResetOTP />} />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   {/* <RegistrationOTP/> */}
+    //   <Registrationform/>
+    //   {/* <LoginForm/> */}
+    //   {/* <PasswordResetOTP/> */}
+    //   {/* <PasswordReset/> */}
+    // </div>
+
   );
 }
 
