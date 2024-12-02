1ba25390bb62c41278569450c604c18340b2e431
AvishkaWPA <avishkabiz123@gmail.com>
2024-12-03 00:14:11 +0530

    added routes inside api gateway

--- oss-backend/apigateway/src/main/java/com/example/apigateway/routes/Routes.java
+++ oss-backend/apigateway/src/main/java/com/example/apigateway/routes/Routes.java
@@ -11,24 +11,28 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @Slf4j
 public class Routes {
-    
+
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         log.info("Configuring gateway routes...");
-        
         RouteLocator routeLocator = builder.routes()
-                    .route("auth-service", r -> r.path("/auth-service/v3/api-docs").uri("lb://auth"))
-                    .route("cart-service", r -> r.path("/cart-service/v3/api-docs").uri("lb://cart"))
-                    .route("delivery-service", r -> r.path("/delivery-service/v3/api-docs").uri("lb://delivery"))
-                    .route("inventory-service", r -> r.path("/inventory-service/v3/api-docs").uri("lb://inventory"))
-                    .route("order-service", r -> r.path("/order-service/v3/api-docs").uri("lb://order"))
-                    .route("payment-service", r -> r.path("/payment-service/v3/api-docs").uri("lb://payment"))
-                    .route("product-service", r -> r.path("/product-service/v3/api-docs").uri("lb://product"))
-                    .route("user-service", r -> r.path("/user-service/v3/api-docs").uri("lb://user"))
-                    
-                    .route("product-service", r -> r.path("/api/v1/products").uri("lb://product"))
-                    .build();
-        
+                .route("auth-service", r -> r.path("/auth-service/v3/api-docs").uri("lb://auth"))
+                .route("cart-service", r -> r.path("/cart-service/v3/api-docs").uri("lb://cart"))
+                .route("delivery-service", r -> r.path("/delivery-service/v3/api-docs").uri("lb://delivery"))
+                .route("inventory-service", r -> r.path("/inventory-service/v3/api-docs").uri("lb://inventory"))
+                .route("order-service", r -> r.path("/order-service/v3/api-docs").uri("lb://order"))
+                .route("payment-service", r -> r.path("/payment-service/v3/api-docs").uri("lb://payment-service"))
+                .route("product-service", r -> r.path("/product-service/v3/api-docs").uri("lb://product"))
+                .route("user-service", r -> r.path("/user-service/v3/api-docs").uri("lb://user"))
+                .route("product-service", r -> r.path("/api/v1/products").uri("lb://product"))
+
+
+                .route("payment-service", r -> r.path("/api/v1/payments").uri("lb://payment-service"))
+                .route("payment-service", r -> r.path("/api/v1/payments/{id}").uri("lb://payment-service"))
+                .route("payment-service", r -> r.path("/api/v1/payments/link").uri("lb://payment-service"))
+                .route("payment-service", r -> r.path("/api/v1/payments/page/{pageNo}").uri("lb://payment-service"))
+                .build();
         log.info("Gateway routes configured successfully.");
         return routeLocator;
     }
