b1da65e03518b4da407a4a24a71522a7f0cf5516
AvishkaWPA <avishkabiz123@gmail.com>
2024-12-03 00:11:40 +0530

    add payment repo

--- oss-backend/payment/src/main/java/com/example/payment/repo/PaymentRepo.java
+++ oss-backend/payment/src/main/java/com/example/payment/repo/PaymentRepo.java
@@ -1,6 +1,8 @@
 package com.example.payment.repo;
 
 import com.example.payment.entity.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,8 @@ import org.springframework.stereotype.Repository;
 @EnableJpaRepositories
 public interface PaymentRepo extends JpaRepository<Payment,Integer> {
 
+    boolean existsPaymentByOrderId(int orderId);
+
+
+    Page<Payment> findAll(Pageable pageable);
 }
