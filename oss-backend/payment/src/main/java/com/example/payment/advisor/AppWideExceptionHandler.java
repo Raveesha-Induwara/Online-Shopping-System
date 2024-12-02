17c9f54ff06437058f6274dae6a4b0f7b393aec4
AvishkaWPA <avishkabiz123@gmail.com>
2024-12-03 00:09:46 +0530

    defined custom exceptions

--- oss-backend/payment/src/main/java/com/example/payment/advisor/AppWideExceptionHandler.java
+++ oss-backend/payment/src/main/java/com/example/payment/advisor/AppWideExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.payment.advisor;
 
 import com.example.payment.dto.response.StandardResponse;
+import com.example.payment.exception.CustomRuntimeException;
+import com.example.payment.exception.NotFoundException;
 import com.example.payment.exception.PaymentException;
 import com.example.payment.exception.PaymentProcessingException;
 import org.springframework.http.HttpStatus;
@@ -17,10 +19,22 @@ public class AppWideExceptionHandler {
         return new ResponseEntity<StandardResponse>(new StandardResponse(400,"Error",e.getMessage()),HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(PaymentProcessingException.class)
-    public ResponseEntity<StandardResponse> handlePaymentProcessingException(PaymentProcessingException e){
+//    @ExceptionHandler(PaymentProcessingException.class)
+//    public ResponseEntity<StandardResponse> handlePaymentProcessingException(PaymentProcessingException e){
+//        return new ResponseEntity<StandardResponse>(new StandardResponse(500,"Error",e.getMessage()),HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+
+    @ExceptionHandler(CustomRuntimeException.class)
+    public ResponseEntity<StandardResponse> handleCustomeRuntimeException(CustomRuntimeException e){
         return new ResponseEntity<StandardResponse>(new StandardResponse(500,"Error",e.getMessage()),HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<StandardResponse> handleNotfoundException(NotFoundException e){
+        return new ResponseEntity<StandardResponse>(new StandardResponse(404,"Error",e.getMessage()),HttpStatus.NOT_FOUND);
+    }
+
+
 
 
 }
