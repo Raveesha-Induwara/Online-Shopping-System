c17c4867b500d5191702724afec78a5fa9af7af1
6614Kavya <109873890+6614Kavya@users.noreply.github.com>
2024-12-03 13:57:36 +0530

    Arranged the file structure of feature/frontend/login

--- oss-frontend/src/Layouts/CustomerLayout.tsx
+++ oss-frontend/src/Layouts/CustomerLayout.tsx
@@ -5,10 +5,22 @@ import ProductDetails from "../Pages/Customer/ProductDetails";
 import MyCart from "../Pages/Customer/MyCart";
 import MyOrders from "../Pages/Customer/MyOrders";
 import DeliveryDetails from "../Pages/Customer/DeliveryDetails";
+import RegisterCustomer from "../Pages/Customer/RegisterCustomer";
+import RegistrationOTP from "../Pages/Customer/RegistrationOTP";
+import LoginForm from "../Pages/Customer/UserLogin";
+import PasswordReset from "../Pages/Customer/PasswordReset";
+import PasswordResetOTP from "../Pages/Customer/RestPasswordOTP";
 
 const CustomerLayout: React.FC = () => {
   return (
     <Routes>
+      <Route path="/" element={<RegisterCustomer />} />
+      <Route path="/registrationOTP" element={<RegistrationOTP />} />
+      <Route path="/loginForm" element={<LoginForm />} />
+
+      <Route path="/loginForm" element={<LoginForm />} />
+      <Route path="/passwordReset" element={<PasswordReset />} />
+      <Route path="/passwordResetOTP" element={<PasswordResetOTP />} />
       <Route path="productDetails" element={<ProductDetails />} />
       <Route path="myCart" element={<MyCart />} />
       <Route path="myOrders" element={<MyOrders />} />
---
+++ oss-frontend/src/Pages/Customer/UserLogin.tsx
@@ -0,0 +1,155 @@
+import { useState } from "react";
+import { Container, Button, TextField, Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { NavBar } from "../../Components/NavBar-reg";
+import { useNavigate } from "react-router-dom";
+import "../../App.css";
+
+type FormValue = {
+  email: string;
+  password: string;
+};
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValue>();
+  const [data, setData] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+  const onSubmit: SubmitHandler<FormValue> = (data) => {};
+  const handleForgotPasswordClick = () => {
+    navigate("/customer/passwordResetOTP");
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <div className="bg-color">
+        <Container>
+          <div className="form-wrapper">
+            <Grid>
+              <Grid size={{ xs: 10, sm: 10, lg: 10, md: 10 }}>
+                <Box>
+                  <Typography
+                    sx={{
+                      color: "white",
+                      padding: "10px",
+                      textAlign: "center",
+                      backgroundColor: " #185A9D",
+                      borderRadius: "18px 18px 0px 0px",
+                    }}
+                  >
+                    User Login
+                  </Typography>
+                </Box>
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <Box sx={{ padding: "20px" }}>
+                    <TextField
+                      fullWidth
+                      sx={{
+                        width: "100%",
+                        marginTop: "20px",
+                        "& .MuiOutlinedInput-root": {
+                          "& fieldset": {
+                            borderColor: "gray",
+                          },
+                          "&:hover fieldset": {
+                            borderColor: "gray",
+                          },
+                          "&.Mui-focused fieldset": {
+                            borderColor: "gray",
+                          },
+                        },
+                      }}
+                      size="small"
+                      label="Email"
+                      placeholder="Enter your eamil"
+                      {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                          value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                          message: "Invalid Email",
+                        },
+                      })}
+                    />
+                    {errors.email && (
+                      <p className="error-msg">{errors.email.message}</p>
+                    )}
+                    <TextField
+                      fullWidth
+                      sx={{
+                        width: "100%",
+                        marginTop: "20px",
+                        "& .MuiOutlinedInput-root": {
+                          "& fieldset": {
+                            borderColor: "gray",
+                          },
+                          "&:hover fieldset": {
+                            borderColor: "gray",
+                          },
+                          "&.Mui-focused fieldset": {
+                            borderColor: "gray",
+                          },
+                        },
+                      }}
+                      size="small"
+                      label="Password"
+                      type="password"
+                      placeholder="Enter your password"
+                      {...register("password", {
+                        required: "Password is required",
+                        minLength: {
+                          value: 6,
+                          message: "Password should be atleast 6 characters",
+                        },
+                      })}
+                    />
+                    {errors.password && (
+                      <p className="error-msg">{errors.password.message}</p>
+                    )}
+
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "flex-end",
+                        alignItems: "center",
+                        marginTop: 1,
+                        color: "grey",
+                      }}
+                    >
+                      <Typography
+                        // variant="h6"
+                        fontSize={15}
+                        // component="div"
+                        sx={{ cursor: "pointer" }}
+                        onClick={handleForgotPasswordClick}
+                      >
+                        Forgot Password
+                      </Typography>
+                    </Box>
+
+                    <Button
+                      style={{ marginTop: "20px" }}
+                      sx={{ backgroundColor: "#185A9D" }}
+                      type="submit"
+                      variant="contained"
+                      fullWidth
+                    >
+                      Login
+                    </Button>
+                  </Box>
+                </form>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+export default LoginForm;
